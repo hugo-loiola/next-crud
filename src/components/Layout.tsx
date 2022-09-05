de0aecf690345395ad5e6c4023acf0008767aa59
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-09-05 17:19:01 -0300

    CRUD com nextjs, tailwind e typescript

---
+++ src/components/Layout.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+interface LayoutProps{
+  titulo: string
+  children: any
+}
+
+const Layout = (props: LayoutProps) => {
+  return (
+    <div className={`
+    flex flex-col w-2/3
+    bg-white text-gray-800
+    rounded-md
+    `}>
+
+      <Titulo>{props.titulo}</Titulo>
+
+      <div className="p-5">
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <div className={`
+    flex justify-center items-center h-screen
+    bg-gradient-to-r from-blue-400 to-purple-500
+    text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo</span>
+      </Layout>
+    </div>
+  )
+}
