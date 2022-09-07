0c44379f6f26885352833577a40ef74a72428549
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-09-07 12:55:46 -0300

    Formulario inicializado

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Formulario = props => {
+  return (
+    <div>Formulario</div>
+  )
+}
+
+Formulario.propTypes = {}
+
+export default Formulario
